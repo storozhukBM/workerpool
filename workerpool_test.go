e341ff4f33f751c0f2684f12f1f0ff8a7e0d3e12
bohdanstorozhuk <storozhuk.b.m@gmail.com>
2021-12-02 00:31:00 +0000

    More tests for pool limits

--- workerpool_test.go
+++ workerpool_test.go
@@ -189,6 +189,220 @@ func TestWorkerPool(t *testing.T) {
 	eq(t, lastCycleNumber+1, wp.AdaptationsExecuted())
 	eq(t, 1, runtime.NumGoroutine()-startG)
 	eq(t, int64(2*workers+(workers/2))+2, atomic.LoadInt64(&jobsDone))
+
+	waitUntil(t, func() bool { return wp.WorkersInPool() == 0 }, 2*statsRefreshPeriod)
+}
+
+func TestWorkerPoolStaysWithinWorkerPoolLimits(t *testing.T) {
+	const statsRefreshPeriod = 250 * time.Millisecond
+	const shortDelay = 50 * time.Millisecond
+	const maxWorkers = 10
+	const minWorkers = 5
+	wp := workerpool.NewWithOptions(minWorkers, maxWorkers, statsRefreshPeriod)
+	eq(t, 0, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, maxWorkers, wp.TargetWorkersInPool())
+	eq(t, 0, wp.MaxConcurrencyObservedInCycle())
+	eq(t, 0, wp.AdaptationsExecuted())
+
+	startG := runtime.NumGoroutine()
+	jobsDone := int64(0)
+
+	// submit tasks and grow till `workers` goroutines
+	// create twice more workers than expected
+	for i := 1; i <= 2*maxWorkers; i++ {
+		wp.Do(func() {
+			time.Sleep(shortDelay)
+			atomic.AddInt64(&jobsDone, 1)
+		})
+		waitUntil(t, func() bool { return wp.CurrentRunningWorkers() == i }, shortDelay/maxWorkers)
+		eq(t, 0, wp.WorkersInPool())
+		eq(t, i, wp.CurrentRunningWorkers())
+		eq(t, maxWorkers, wp.TargetWorkersInPool())
+		eq(t, i, wp.MaxConcurrencyObservedInCycle())
+		eq(t, 0, wp.AdaptationsExecuted())
+		eq(t, i, runtime.NumGoroutine()-startG)
+		eq(t, int64(0), atomic.LoadInt64(&jobsDone))
+	}
+
+	// wait for tasks to finish and check that only max waiting workers are still waiting for new tasks
+	waitUntil(t, func() bool { return atomic.LoadInt64(&jobsDone) == 2*maxWorkers }, 2*shortDelay)
+	eq(t, maxWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, maxWorkers, wp.TargetWorkersInPool())
+	eq(t, 2*maxWorkers, wp.MaxConcurrencyObservedInCycle())
+	eq(t, 0, wp.AdaptationsExecuted())
+	eq(t, maxWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(2*maxWorkers), atomic.LoadInt64(&jobsDone))
+
+	// wait for the first adaption cycle and check that target workers in pool remains
+	waitUntil(t, func() bool { return wp.AdaptationsExecuted() == 1 }, statsRefreshPeriod)
+	eq(t, maxWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, maxWorkers, wp.TargetWorkersInPool())
+	eq(t, 0, wp.MaxConcurrencyObservedInCycle())
+	eq(t, 1, wp.AdaptationsExecuted())
+	eq(t, maxWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(2*maxWorkers), atomic.LoadInt64(&jobsDone))
+
+	// run less tasks
+	for i := 1; i <= minWorkers/2; i++ {
+		wp.Do(func() {
+			time.Sleep(shortDelay)
+			atomic.AddInt64(&jobsDone, 1)
+		})
+		waitUntil(t, func() bool { return wp.CurrentRunningWorkers() == i }, shortDelay/minWorkers)
+		eq(t, maxWorkers, wp.WorkersInPool())
+		eq(t, i, wp.CurrentRunningWorkers())
+		eq(t, maxWorkers, wp.TargetWorkersInPool())
+		eq(t, i, wp.MaxConcurrencyObservedInCycle())
+		eq(t, 1, wp.AdaptationsExecuted())
+		eq(t, maxWorkers, runtime.NumGoroutine()-startG)
+		eq(t, int64(2*maxWorkers), atomic.LoadInt64(&jobsDone))
+	}
+
+	// wait for tasks to finish and check that only target waiting workers are still waiting for new tasks
+	waitUntil(t, func() bool { return atomic.LoadInt64(&jobsDone) == 2*maxWorkers+(minWorkers/2) }, 2*shortDelay)
+	eq(t, maxWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, maxWorkers, wp.TargetWorkersInPool())
+	eq(t, minWorkers/2, wp.MaxConcurrencyObservedInCycle())
+	eq(t, 1, wp.AdaptationsExecuted())
+	eq(t, maxWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(2*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+
+	// wait for tasks to finish and check that adaptation happens
+	waitUntil(t, func() bool { return wp.AdaptationsExecuted() == 2 }, statsRefreshPeriod)
+	{
+		previousTarget := maxWorkers
+		maxConcurrencyOnThisCycle := minWorkers / 2
+		middlePointBetweenPrevTargetAndNewOne := (previousTarget + maxConcurrencyOnThisCycle) / 2
+		newTarget := middlePointBetweenPrevTargetAndNewOne
+		// check that target workers drop to new target
+		waitUntil(t, func() bool { return wp.WorkersInPool() == newTarget }, 2*shortDelay)
+		eq(t, 0, wp.CurrentRunningWorkers())
+		eq(t, newTarget, wp.TargetWorkersInPool())
+		eq(t, 0, wp.MaxConcurrencyObservedInCycle())
+		eq(t, newTarget, runtime.NumGoroutine()-startG)
+		eq(t, 2, wp.AdaptationsExecuted())
+		eq(t, int64(2*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+	}
+
+	// wait for pool to drop to minimum
+	waitUntil(t, func() bool { return wp.TargetWorkersInPool() == minWorkers }, 3*statsRefreshPeriod)
+	eq(t, minWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, minWorkers, wp.TargetWorkersInPool())
+	eq(t, 0, wp.MaxConcurrencyObservedInCycle())
+	eq(t, minWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(2*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+
+	// check that pool remains on minimum
+	pivotCycle := wp.AdaptationsExecuted()
+	waitUntil(t, func() bool { return wp.AdaptationsExecuted() == pivotCycle+1 }, 2*statsRefreshPeriod)
+	eq(t, minWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, minWorkers, wp.TargetWorkersInPool())
+	eq(t, 0, wp.MaxConcurrencyObservedInCycle())
+	eq(t, minWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(2*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+
+	// submit tasks and grow till `workers` goroutines
+	// create twice more workers than expected
+	for i := 1; i <= 2*maxWorkers; i++ {
+		wp.Do(func() {
+			time.Sleep(shortDelay)
+			atomic.AddInt64(&jobsDone, 1)
+		})
+		waitUntil(t, func() bool { return wp.CurrentRunningWorkers() == i }, shortDelay/maxWorkers)
+		eq(t, minWorkers, wp.WorkersInPool())
+		eq(t, i, wp.CurrentRunningWorkers())
+		eq(t, minWorkers, wp.TargetWorkersInPool())
+		eq(t, i, wp.MaxConcurrencyObservedInCycle())
+		eq(t, pivotCycle+1, wp.AdaptationsExecuted())
+		eq(t, max(minWorkers, i), runtime.NumGoroutine()-startG)
+		eq(t, int64(2*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+	}
+
+	// wait for tasks to finish and check that only target waiting workers are still waiting for new tasks
+	waitUntil(t, func() bool { return atomic.LoadInt64(&jobsDone) == 4*maxWorkers+(minWorkers/2) }, 2*shortDelay)
+	eq(t, minWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, minWorkers, wp.TargetWorkersInPool())
+	eq(t, 2*maxWorkers, wp.MaxConcurrencyObservedInCycle())
+	eq(t, pivotCycle+1, wp.AdaptationsExecuted())
+	eq(t, minWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(4*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+
+	// check that pool goes back to maximum
+	waitUntil(t, func() bool { return wp.AdaptationsExecuted() == pivotCycle+2 }, 2*statsRefreshPeriod)
+	eq(t, minWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, maxWorkers, wp.TargetWorkersInPool())
+	eq(t, 0, wp.MaxConcurrencyObservedInCycle())
+	eq(t, minWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(4*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+
+	// submit tasks and grow till `workers` goroutines
+	// create twice more workers than expected
+	for i := 1; i <= 2*maxWorkers; i++ {
+		wp.Do(func() {
+			time.Sleep(shortDelay)
+			atomic.AddInt64(&jobsDone, 1)
+		})
+		waitUntil(t, func() bool { return wp.CurrentRunningWorkers() == i }, shortDelay/maxWorkers)
+		eq(t, minWorkers, wp.WorkersInPool())
+		eq(t, i, wp.CurrentRunningWorkers())
+		eq(t, maxWorkers, wp.TargetWorkersInPool())
+		eq(t, i, wp.MaxConcurrencyObservedInCycle())
+		eq(t, pivotCycle+2, wp.AdaptationsExecuted())
+		eq(t, max(minWorkers, i), runtime.NumGoroutine()-startG)
+		eq(t, int64(4*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+	}
+
+	// wait for tasks to finish and check that only target waiting workers are still waiting for new tasks
+	waitUntil(t, func() bool { return atomic.LoadInt64(&jobsDone) == 6*maxWorkers+(minWorkers/2) }, 2*shortDelay)
+	eq(t, maxWorkers, wp.WorkersInPool())
+	eq(t, 0, wp.CurrentRunningWorkers())
+	eq(t, maxWorkers, wp.TargetWorkersInPool())
+	eq(t, 2*maxWorkers, wp.MaxConcurrencyObservedInCycle())
+	eq(t, pivotCycle+2, wp.AdaptationsExecuted())
+	eq(t, maxWorkers, runtime.NumGoroutine()-startG)
+	eq(t, int64(6*maxWorkers+(minWorkers/2)), atomic.LoadInt64(&jobsDone))
+}
+
+func TestWorkerParamsValidation(t *testing.T) {
+	t.Parallel()
+	workerpool.New()
+	workerpool.NewWithOptions(0, 1, time.Millisecond)
+	workerpool.NewWithOptions(math.MaxInt32, math.MaxInt32, math.MaxInt64*time.Nanosecond)
+
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(0, 0, time.Second)
+	}, "workerpool: maxWorkersInPool should be between 1 and math.MaxInt32")
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(0, -1, time.Second)
+	}, "workerpool: maxWorkersInPool should be between 1 and math.MaxInt32")
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(0, int(math.MaxInt32)+1, time.Second)
+	}, "workerpool: maxWorkersInPool should be between 1 and math.MaxInt32")
+
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(-1, 1, time.Second)
+	}, "workerpool: minWorkersInPool should be between 0 and math.MaxInt32 and not bigger than maxWorkersInPool")
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(int(math.MaxInt32)+1, 1, time.Second)
+	}, "workerpool: minWorkersInPool should be between 0 and math.MaxInt32 and not bigger than maxWorkersInPool")
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(10, 9, time.Second)
+	}, "workerpool: minWorkersInPool should be between 0 and math.MaxInt32 and not bigger than maxWorkersInPool")
+
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(0, 1, time.Millisecond-time.Nanosecond)
+	}, "workerpool: adaptationPeriod should be at least 1 millisecond")
+	expectPanic(t, func() {
+		workerpool.NewWithOptions(0, 1, time.Duration(0))
+	}, "workerpool: adaptationPeriod should be at least 1 millisecond")
 }
 
 func TestVisualisationExponentialDeclineOfWaitingTime(t *testing.T) {
@@ -227,38 +441,6 @@ func TestVisualisationExponentialDeclineOfWaitingTime(t *testing.T) {
 	}
 }
 
-func TestWorkerParamsValidation(t *testing.T) {
-	workerpool.NewWithOptions(0, 1, time.Millisecond)
-	workerpool.NewWithOptions(math.MaxInt32, math.MaxInt32, math.MaxInt64*time.Nanosecond)
-
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(0, 0, time.Second)
-	}, "workerpool: maxWorkersInPool should be between 1 and math.MaxInt32")
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(0, -1, time.Second)
-	}, "workerpool: maxWorkersInPool should be between 1 and math.MaxInt32")
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(0, int(math.MaxInt32)+1, time.Second)
-	}, "workerpool: maxWorkersInPool should be between 1 and math.MaxInt32")
-
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(-1, 1, time.Second)
-	}, "workerpool: minWorkersInPool should be between 0 and math.MaxInt32 and not bigger than maxWorkersInPool")
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(int(math.MaxInt32)+1, 1, time.Second)
-	}, "workerpool: minWorkersInPool should be between 0 and math.MaxInt32 and not bigger than maxWorkersInPool")
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(10, 9, time.Second)
-	}, "workerpool: minWorkersInPool should be between 0 and math.MaxInt32 and not bigger than maxWorkersInPool")
-
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(0, 1, time.Millisecond-time.Nanosecond)
-	}, "workerpool: adaptationPeriod should be at least 1 millisecond")
-	expectPanic(t, func() {
-		workerpool.NewWithOptions(0, 1, time.Duration(0))
-	}, "workerpool: adaptationPeriod should be at least 1 millisecond")
-}
-
 func expectPanic(t *testing.T, f func(), expectedError string) {
 	t.Helper()
 	var actualPanic interface{}
@@ -293,4 +475,11 @@ func eq(t *testing.T, expected interface{}, actual interface{}) {
 	if !reflect.DeepEqual(expected, actual) {
 		t.Fatalf("\nexp: %T:`%#v`\nact: %T:`%#v`", expected, expected, actual, actual)
 	}
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
